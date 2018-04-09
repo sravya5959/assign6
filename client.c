4c5b593c2708fc9a054ec502e84209e41aa53c15
sravya5959 <35134992+sravya5959@users.noreply.github.com>
2018-04-09 18:38:49 +0530

    Add files via upload

--- client.c
+++ client.c
@@ -8,19 +8,46 @@
 #include <unistd.h>
 #include <errno.h>
 #include <arpa/inet.h> 
+#include <pthread.h> 
+#include <semaphore.h> 
 
+int sockfd = 0, n = 0;
+char recvBuff[10240];
+char sendBuff[10240];
+struct sockaddr_in serv_addr;
+char *message;
+
+void *Client()
+{
+	while(1)
+	{
+		//printf("client: ");
+	    fgets(message,50,stdin);  
+	    strcpy(sendBuff,message);
+	    write(sockfd, sendBuff, strlen(sendBuff));
+	}
+}
+
+void *Server()
+{
+	while(1)
+	{
+		n = read(sockfd, recvBuff, sizeof(recvBuff)-1);
+	    
+	   // printf("server: "); 	
+	    recvBuff[n] = 0;
+	    printf("server: %s",recvBuff );
+	}
+}
 int main(int argc, char *argv[])
 {
-    int sockfd = 0, n = 0;
-    char recvBuff[10240];
-    char sendBuff[10240];
-    struct sockaddr_in serv_addr; 
-
-    if(argc != 2)
+    
+	message= (char*)malloc(sizeof(char)*10240);
+ /*   if(argc != 2)
     {
         printf("\n Usage: %s <ip of server> \n",argv[0]);
         return 1;
-    } 
+    }*/ 
 
     memset(recvBuff, '0',sizeof(recvBuff));
     if((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
@@ -34,7 +61,7 @@ int main(int argc, char *argv[])
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(5400); 
 
-    if(inet_pton(AF_INET, argv[1], &serv_addr.sin_addr)<=0)
+    if(inet_pton(AF_INET,"127.0.0.1", &serv_addr.sin_addr)<=0)
     {
         printf("\n inet_pton error occured\n");
         return 1;
@@ -45,31 +72,14 @@ int main(int argc, char *argv[])
        printf("\n Error : Connect Failed \n");
        return 1;
     } 
-    char *message= (char*)malloc(sizeof(char)*10240);
-    int i;
-    
-    while (1)
-    {
-    	printf("client: ");
-    	
-    	fgets(message,50,stdin);
-      
-        strcpy(sendBuff,message);
-        write(sockfd, sendBuff, strlen(sendBuff));
-       
-        printf("server: "); 
-    	n = read(sockfd, recvBuff, sizeof(recvBuff)-1);
-        recvBuff[n] = 0;
-        printf("%s",recvBuff );
-       
-        sleep(1);
-        
-    } 
+   
+   pthread_t read,write;
+   pthread_create(&write,NULL,Client,NULL);
+   pthread_create(&read,NULL,Server,NULL);
 
-    if(n < 0)
-    {
-        printf("\n Read error \n");
-    }
+   pthread_join(write,NULL);
+   pthread_join(read,NULL); 
+
 
     return 0;
 }
--- server.c
+++ server.c
@@ -8,15 +8,41 @@
 #include <string.h>
 #include <sys/types.h>
 #include <time.h> 
+#include <pthread.h> 
+#include <semaphore.h> 
+
+int listenfd = 0, connfd = 0,n=0;
+struct sockaddr_in serv_addr; 
+char * message;
+char sendBuff[10250];
+char recvBuff[10250];
+char *msg;
+
+void *Client()
+{
+    while(1)
+    {
+        //printf("client: ");
+        n = read(connfd, recvBuff, sizeof(recvBuff)-1);
+        recvBuff[n] = 0;
+        printf("client:%s",recvBuff);
+    }
+}
+
+void *Server()
+{
+    while(1)
+    {
+        //printf("server: ");
+        fgets(msg,50,stdin);
+        strcpy(sendBuff,msg);
+        write(connfd, sendBuff, strlen(sendBuff));
+    }
+}
 
 int main(int argc, char *argv[])
 {
-    int listenfd = 0, connfd = 0,n=0;
-    struct sockaddr_in serv_addr; 
-    char * message;
-    char sendBuff[10250];
-    char recvBuff[10250];
-    char *msg=(char *)malloc(sizeof(char)*10240);
+    msg=(char *)malloc(sizeof(char)*10240);
     listenfd = socket(AF_INET, SOCK_STREAM, 0);
     memset(&serv_addr, '0', sizeof(serv_addr));
     memset(sendBuff, '0', sizeof(sendBuff)); 
@@ -30,24 +56,15 @@ int main(int argc, char *argv[])
     listen(listenfd, 10); 
 
     connfd = accept(listenfd, (struct sockaddr*)NULL, NULL); 
-    int i;
-    while(1)
-    {
-        printf("client: ");
-        n = read(connfd, recvBuff, sizeof(recvBuff)-1);
-        recvBuff[n] = 0;
-        printf("%s",recvBuff);
-       
-        printf("server: ");
-        fgets(msg,50,stdin);
-       
-        strcpy(sendBuff,msg);
-        write(connfd, sendBuff, strlen(sendBuff)); 
-        sleep(1);
-    }
-    close(connfd);
-    sleep(1);
+    
+    //close(connfd);
+    pthread_t read,write;
+    pthread_create(&read,NULL,Client,NULL);
+   pthread_create(&write,NULL,Server,NULL);
+
+   pthread_join(read,NULL);
+   pthread_join(write,NULL); 
         
-        
+    return 0;
 
 }
